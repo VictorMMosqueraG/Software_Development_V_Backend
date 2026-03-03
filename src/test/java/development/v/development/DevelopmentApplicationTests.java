03b4cb01a713feb87f14eb8eedefda4d45d4fc51
VictorMMosqueraG <victormmosquerag@gmail.com>
2026-03-03 18:43:53 -0500

    first commit

---
+++ src/test/java/development/v/development/DevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package development.v.development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
