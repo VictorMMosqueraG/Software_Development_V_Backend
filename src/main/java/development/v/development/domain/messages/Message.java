6810ac4f93ae5e484eb6b2fe4a229dd81b33bf5e
VictorMMosqueraG <victormmosquerag@gmail.com>
2026-03-04 19:48:28 -0500

    semantic version

--- src/main/java/development/v/development/domain/messages/Message.java
+++ src/main/java/development/v/development/domain/messages/Message.java
@@ -1,7 +1,7 @@
 package development.v.development.domain.messages;
 
 public class Message {
-    public static final String VERSION_API_SWAGGER = "v1.0.0";
+    public static final String VERSION_API_SWAGGER = "v1.2.0";
     public static final String DESCRIPTION_API_SWAGGER = "API REST para la gestión del restaurante";
     public static final String TITTLE_API_SWAGGER = "Restaurant API";
 
